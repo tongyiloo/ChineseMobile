a71e5044c9b4851cb42609c9c2523d4d5ca06b43
tongyiloo <84721358+tongyiloo@users.noreply.github.com>
2021-10-14 02:45:58 +0800

    Register main

--- app/src/main/java/com/chinesemobile/chinesemobile/LoginActivity.java
+++ app/src/main/java/com/chinesemobile/chinesemobile/LoginActivity.java
@@ -2,13 +2,29 @@ package com.chinesemobile.chinesemobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.chinesemobile.chinesemobile.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //handle click, go to register screen
+        binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
