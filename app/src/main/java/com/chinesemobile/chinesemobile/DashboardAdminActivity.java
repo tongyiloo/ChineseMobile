8f42b0d1d10f21cc79e1af7991db12dec0916121
tongyiloo <84721358+tongyiloo@users.noreply.github.com>
2021-10-17 23:24:47 +0800

    show categories

--- app/src/main/java/com/chinesemobile/chinesemobile/DashboardAdminActivity.java
+++ app/src/main/java/com/chinesemobile/chinesemobile/DashboardAdminActivity.java
@@ -1,14 +1,25 @@
 package com.chinesemobile.chinesemobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Adapter;
 
 import com.chinesemobile.chinesemobile.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -18,6 +29,12 @@ public class DashboardAdminActivity extends AppCompatActivity {
     //firebase
     private FirebaseAuth firebaseAuth;
 
+    //arrayList to store category
+    private ArrayList<ModelCategory> categoryArrayList;
+    //adapter
+    private AdapterCategory adapterCategory;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +43,32 @@ public class DashboardAdminActivity extends AppCompatActivity {
         
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
+
+        //edit text change listern,search
+        binding.searchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //called as and when user type each letter
+                try {
+                    adapterCategory.getFilter().filter(s);
+                }
+                catch (Exception e){
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         //handle click, logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +84,37 @@ public class DashboardAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DashboardAdminActivity.this, VocabularyCategoryAddActivity.class));
+            }
+        });
+    }
+
+    private void loadCategories() {
+        //init arraylist
+        categoryArrayList = new ArrayList<>();
+
+        //get all categories from firebase > Categories
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear arraylist before adding data into it
+                categoryArrayList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    //add to arraylist
+                    categoryArrayList.add(model);
+                }
+                //setup adapter
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this,categoryArrayList);
+                //set adapter to recyclerView
+                binding.categoriesRv.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
---
+++ app/src/main/java/com/chinesemobile/chinesemobile/ModelCategory.java
@@ -0,0 +1,53 @@
+package com.chinesemobile.chinesemobile;
+
+public class ModelCategory {
+    //make sure to use same spelling for model variables in firebase
+
+    String id, category, uid;
+    String timestamp;
+
+    //constructor empty required for firebase
+    public ModelCategory() {
+
+    }
+
+    //parametrized constructor
+    public ModelCategory(String id, String category, String uid, String timestamp) {
+        this.id = id;
+        this.category = category;
+        this.uid = uid;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
